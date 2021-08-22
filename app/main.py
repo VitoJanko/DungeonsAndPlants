3111882a8b9558f44bc9817a2fda1a3893866511
Vito Janko <vito.janko@ijs.si>
2021-08-22 18:04:18 +0200

    Test

--- app/main.py
+++ app/main.py
@@ -3,3 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 from app import views
+
+@app.route("/")
+def home_view():
+        return "<h1>Welcome to Geeks for Geeks</h1>"
+
+@app.route("/about")
+def home_view():
+        return "<h1>Welcome to Geeks for Geeks 22</h1>"
